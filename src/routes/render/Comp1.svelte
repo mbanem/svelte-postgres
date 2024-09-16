09e531d67ca1d3e8fca611e7629588b8d8f63a11
mili <mili.comrunner@gmail.com>
2024-09-16 14:34:28 -0400

    Render page Comp1.svelte turn to Svelte5

--- src/routes/render/Comp1.svelte
+++ src/routes/render/Comp1.svelte
@@ -4,16 +4,7 @@
 		$props()
 </script>
 
-<!-- just markup without TypeScript using $$props -->
 <div class={cls} onclick={() => console.log('clicked')} aria-hidden={true}>
 	{caption1 ?? 'Child'}
 	{@render children()}
 </div>
-<!-- <script>
-	// with script extracting props first and then rendering markup
-	let {cls, caption, children} = $props()
-</script>
-<div class={cls} onclick={console.log('clicked')} aria-hidden={true}>
-{caption ?? 'Child'}
-	{@render children()}
-</div> -->
