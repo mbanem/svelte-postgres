c66594175394a53235ee4fc57f1a68df4787f8b8
mili <mili.comrunner@gmail.com>
2024-11-14 20:07:38 -0500

    Perm Handler added to navbar

--- src/routes/navbar/+page.svelte
+++ src/routes/navbar/+page.svelte
@@ -1,143 +1,152 @@
 <script lang="ts">
-	import NavButton from '$lib/components/NavButton.svelte'
+  import NavButton from '$lib/components/NavButton.svelte';
 
-	const items = [
-		{ color: 'Pink', title: 'Home', href: '/' },
-		{ color: 'Blue', title: 'Derived', href: '/derived' },
-		{ title: 'Scroll', href: '/scroll' }, // make one with no color to use default color -- red
-		{ color: 'Tomato', title: 'Caterpillar', href: '/caterpillar' },
-		{ color: 'Green', title: 'Balloon', href: '/balloon' },
-		{ color: 'Purple', title: 'Render', href: '/render' },
-		{ color: 'LightGreen', title: 'Bars', href: '/bars' },
-		{ color: 'Pink', title: 'Bindable', href: '/bindable' },
-		{ color: 'SkyBlue', title: 'Communicate', href: '/communicate' },
-		{ color: 'ForestGreen', title: 'ContactCard', href: '/contact' },
-		{ color: 'Yellow', title: 'Duck-Goose', href: '/duck_goose' },
-		{ color: 'Rebeccapurple', title: 'Themes', href: '/themes' },
-		{ color: 'Red', title: 'Floating Labels', href: '/floating_label' },
-		{ color: 'LightGreen', title: 'Before-Input', href: '/before_input' },
-		{ color: 'Pink', title: 'A-Button', href: '/a-button' },
-		{ color: 'Green', title: 'Box', href: '/box' },
-		{ color: 'Yellow', title: 'Flip', href: '/flip' }
-	]
+  const items = [
+    { color: 'Pink', title: 'Home', href: '/' },
+    { color: 'Blue', title: 'Derived', href: '/derived' },
+    { title: 'Scroll', href: '/scroll' }, // make one with no color to use default color -- red
+    { color: 'Tomato', title: 'Caterpillar', href: '/caterpillar' },
+    { color: 'Green', title: 'Balloon', href: '/balloon' },
+    { color: 'Purple', title: 'Render', href: '/render' },
+    { color: 'LightGreen', title: 'Bars', href: '/bars' },
+    { color: 'Pink', title: 'Bindable', href: '/bindable' },
+    { color: 'SkyBlue', title: 'Communicate', href: '/communicate' },
+    { color: 'ForestGreen', title: 'ContactCard', href: '/contact' },
+    { color: 'Yellow', title: 'Duck-Goose', href: '/duck_goose' },
+    { color: 'Rebeccapurple', title: 'Themes', href: '/themes' },
+    { color: 'Red', title: 'Floating Labels', href: '/floating_label' },
+    { color: 'LightGreen', title: 'Before-Input', href: '/before_input' },
+    { color: 'Pink', title: 'A-Button', href: '/a-button' },
+    { color: 'Green', title: 'Box', href: '/box' },
+    { color: 'Pink', title: 'Perm Handler', href: '/permissions' },
+    { color: 'Yellow', title: 'Flip', href: '/flip' },
+  ];
 
-	// using $effect
-	import { tick } from 'svelte'
-	let inputBox: HTMLInputElement
-	let div: HTMLDivElement
-	let messages = $state<string[]>([])
+  // using $effect
+  import { tick } from 'svelte';
+  let inputBox: HTMLInputElement;
+  let div: HTMLDivElement;
+  let messages = $state<string[]>([]);
 
-	$effect.pre(() => {
-		if (!div) return // not yet mountedChip
-		// reference `messages` array length so that this code re-runs whenever it changes
-		messages.length
-		// auto scroll when new messages are added
-		if (div.offsetHeight + div.scrollTop > div.scrollHeight - 20) {
-			tick().then(() => {
-				div?.scrollTo(0, div.scrollHeight)
-			})
-		}
-	})
+  $effect.pre(() => {
+    if (!div) return; // not yet mountedChip
+    // reference `messages` array length so that this code re-runs whenever it changes
+    messages.length;
+    // auto scroll when new messages are added
+    if (div.offsetHeight + div.scrollTop > div.scrollHeight - 20) {
+      tick().then(() => {
+        div?.scrollTo(0, div.scrollHeight);
+      });
+    }
+  });
 
-	const change = (event: KeyboardEvent) => {
-		const char = event.key
-		// console.log(char)
-		if (event.key !== 'Enter') return
-		if (event.target) {
-			messages.push(inputBox.value as string)
-			inputBox.value = ''
-		}
-	}
+  const change = (event: KeyboardEvent) => {
+    const char = event.key;
+    // console.log(char)
+    if (event.key !== 'Enter') return;
+    if (event.target) {
+      messages.push(inputBox.value as string);
+      inputBox.value = '';
+    }
+  };
 </script>
 
 <svelte:head>
-	<title>Navigation Bar</title>
+  <title>Navigation Bar</title>
 </svelte:head>
 <!-- snipper default color is red -->
 {#snippet icon(color = 'Red')}
-	<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" width="16">
-		<circle cx="8" cy="8" r="8" fill={color} />
-	</svg>
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" width="16">
+    <circle cx="8" cy="8" r="8" fill={color} />
+  </svg>
 {/snippet}
 <!-- snippet with default icon of Svelte -->
-{#snippet profile(url = 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg')}
-	<img src={url} width="16" height="16" alt="pfp" />
+{#snippet profile(
+  url = 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg',
+)}
+  <img src={url} width="16" height="16" alt="pfp" />
 {/snippet}
 {#snippet snippetName()}
-	<p>This is the snippet content</p>
+  <p>This is the snippet content</p>
 {/snippet}
 {@render snippetName()}
 <!-- <NavButton label="home" title="Home" href="/" {snippetName}></NavButton> -->
 {#each items as item}
-	<p>
-		<NavButton colorName={item.color ?? 'Red'} title={item.title} href={item.href}>
-			{#snippet media()}
-				{#if item.color}
-					{@render icon(`${item.color}`)}
-				{:else}
-					{@render icon()}
-				{/if}
-			{/snippet}
-		</NavButton>
-	</p>
+  <p>
+    <NavButton
+      colorName={item.color ?? 'Red'}
+      title={item.title}
+      href={item.href}
+    >
+      {#snippet media()}
+        {#if item.color}
+          {@render icon(`${item.color}`)}
+        {:else}
+          {@render icon()}
+        {/if}
+      {/snippet}
+    </NavButton>
+  </p>
 {/each}
 
 <NavButton title="Svelte" href="/button">
-	{#snippet media()}
-		{@render profile()}
-	{/snippet}
+  {#snippet media()}
+    {@render profile()}
+  {/snippet}
 </NavButton>
 <NavButton title="Vue" href="/counter">
-	{#snippet media()}
-		{@render profile('https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg')}
-	{/snippet}
+  {#snippet media()}
+    {@render profile(
+      'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg',
+    )}
+  {/snippet}
 </NavButton>
 
 <pre>Using $effect based on messages.length to render
 	a list of values entered in the below input box
 </pre>
 <input
-	type="text"
-	placeholder="Enter item for the list and press Enter key"
-	size="20"
-	class="input-box"
-	bind:this={inputBox}
-	onkeydown={change}
+  type="text"
+  placeholder="Enter item for the list and press Enter key"
+  size="20"
+  class="input-box"
+  bind:this={inputBox}
+  onkeydown={change}
 />
 <div bind:this={div}>
-	{#each messages as message, index}
-		<p class="names">{index > 0 ? ', ' : ''} {message}</p>
-	{/each}
+  {#each messages as message, index}
+    <p class="names">{index > 0 ? ', ' : ''} {message}</p>
+  {/each}
 </div>
 
 <!-- using $effect -->
 <style>
-	.input-box {
-		width: 20rem;
-	}
-	svg {
-		display: inline-flex;
-		margin-right: 8px;
-	}
-	p {
-		/* must engulf the <Chip> in order to cover it for mouse click */
-		display: inline-block;
-		margin: 0;
-		padding: 3px 6px;
-		color: lightgreen;
-		border-top: 1px solid gray;
-		border-bottom: 1px solid gray;
-	}
-	p.names {
-		color: yellow;
-		padding: 0;
-		margin: 0;
-	}
-	:global(.greeting) {
-		font-style: italic;
-		:hover {
-			color: yellow;
-			border-color: yellow;
-		}
-	}
+  .input-box {
+    width: 20rem;
+  }
+  svg {
+    display: inline-flex;
+    margin-right: 8px;
+  }
+  p {
+    /* must engulf the <Chip> in order to cover it for mouse click */
+    display: inline-block;
+    margin: 0;
+    padding: 3px 6px;
+    color: lightgreen;
+    border-top: 1px solid gray;
+    border-bottom: 1px solid gray;
+  }
+  p.names {
+    color: yellow;
+    padding: 0;
+    margin: 0;
+  }
+  :global(.greeting) {
+    font-style: italic;
+    :hover {
+      color: yellow;
+      border-color: yellow;
+    }
+  }
 </style>
